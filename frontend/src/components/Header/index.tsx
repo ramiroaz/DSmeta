4625023141bbb4a4ece9716e2ba14fa2a50e2ef4
Ramiro <ramiroaz@gmail.com>
2022-07-12 19:34:52 -0300

    Outros Componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DS Meta Dashboard" />
+        <h1>DS Meta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://linkedin.com/in/ramiro-arce"> Ramiro Zavala</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
