1ebee9070c9d7ba15553e2bf7df78c3f25f5b70e
Ramiro <ramiroaz@gmail.com>
2022-07-14 18:56:50 -0300

    Twilio SMS v2

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,8 +35,11 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
+//		String msg = "O vendedor : " + sale.getSellerName() + "foi destaque de vendas em " + date + 
+//				     ", com um total de R$ " + String.format("%.2f", sale.getAmount());
+		
 		String msg = "O vendedor : " + sale.getSellerName() + "foi destaque de vendas em " + date + 
-				     ", com um total de R$ " + String.format("%.2f", sale.getAmount());
+			     ", com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
